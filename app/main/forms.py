78828c9002e1e4db68587a23f33b0538f6802f96
rmautia <raphaelnyangenya@gmail.com>
2020-05-06 00:38:12 +0300

    new submission for each category

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,7 +6,6 @@ class PitchForm(FlaskForm):
 
     title = StringField('Pitch title',validators=[Required()])
     text = TextAreaField('Text')
-    category = SelectField('Type',choices=[('survey','Survey pitch'),('idea','Idea pitch'),('promotion','Promotion pitch')],validators=[Required()])
     submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
@@ -18,12 +17,12 @@ class CommentForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-class PitchForm(FlaskForm):
+# class PitchForm(FlaskForm):
 
- title = StringField('Pitch title',validators=[Required()])
+#  title = StringField('Pitch title',validators=[Required()])
 
- text = TextAreaField('Text')
+#  text = TextAreaField('Text')
 
- category = SelectField ('Type',choices=[('survey','Survey pitch'),('idea','Idea pitch'),('promotion','Promotion pitch')],validators=[Required()])
+#  category = SelectField ('Type',choices=[('survey','Survey pitch'),('idea','Idea pitch'),('promotion','Promotion pitch')],validators=[Required()])
 
- submit = SubmitField('Submit') 
+#  submit = SubmitField('Submit') 
